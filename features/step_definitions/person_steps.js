60c7eaee4e7089a2adc065db52cf20591e1f5750
Jim Barrows <Jim.Barrows@gmail.com>
2017-03-09 06:50:08 -0700

    You can add a blank person, with just an email address.

--- features/step_definitions/person_steps.js
+++ features/step_definitions/person_steps.js
@@ -26,7 +26,11 @@ defineSupportCode(function({
         callback();
     });
     Given('I have provided a date of birth of {date_of_birth:stringInDoubleQuotes}', function(date_of_birth, callback) {
-        this.person.date_of_birth = moment(date_of_birth + "00:00 -0700", "MM-DD-YYYY HH:mm Z");
+        if (date_of_birth) {
+            this.person.date_of_birth = moment(date_of_birth + "00:00 -0700", "MM-DD-YYYY HH:mm Z");
+        } else {
+            this.person.date_of_birth = null;
+        }
         callback();
     });
 
@@ -37,14 +41,15 @@ defineSupportCode(function({
 
     When('I save the person', function() {
         return this.axios.post('/', {
-                "query": "mutation create_person( $first_name: String, $last_name:String, $title:String, $nickname:String, $date_of_birth:String, $comment: String) { create_person(first_name: $first_name, last_name: $last_name, title: $title, nickname: $nickname, date_of_birth: $date_of_birth, comment: $comment){ id } }",
+                "query": "mutation create_person( $first_name: String, $last_name:String, $title:String, $nickname:String, $date_of_birth:String, $comment: String, $email: String) { create_person(first_name: $first_name, last_name: $last_name, title: $title, nickname: $nickname, date_of_birth: $date_of_birth, comment: $comment, email: $email){ id } }",
                 "variables": {
                     "first_name": this.person.first_name,
                     "last_name": this.person.last_name,
                     "title": this.person.title,
                     "nickname": this.person.nickname,
-                    "date_of_birth": this.person.date_of_birth.toJSON(),
-                    "comment": this.person.comment
+                    "date_of_birth": this.person.date_of_birth ? this.person.date_of_birth.toJSON() : null,
+                    "comment": this.person.comment,
+                    "email": this.person.email
                 },
                 "operationName": "create_person"
             })
@@ -52,27 +57,30 @@ defineSupportCode(function({
 
     });
 
-    Then('the data will be in the database', function() {
+    Then('the person data will be in the database', function() {
         expect(this.result.status).to.be.equal(200);
         expect(this.result.data.errors).to.be.falsy;
         expect(this.result.data.data).to.exist;
         expect(this.result.data.data.create_person).to.exist;
         expect(this.result.data.data.create_person.id).to.exist;
-        return this.db.one("select id, first_name, last_name, title, nickname, date_of_birth, comment from person where id=$1", [this.result.data.data.create_person.id])
+        return this.db.one("select id, first_name, last_name, title, nickname, date_of_birth, comment from party where id=$1", [this.result.data.data.create_person.id])
             .then(data => {
                 expect(data.id).to.be.equal(this.result.data.data.create_person.id);
                 expect(data.first_name).to.be.equal(this.person.first_name);
                 expect(data.last_name).to.be.equal(this.person.last_name);
                 expect(data.title).to.be.equal(this.person.title);
                 expect(data.nickname).to.be.equal(this.person.nickname);
-                expect(data.date_of_birth.toJSON()).to.be.equal(this.person.date_of_birth.toJSON());
+                if( data.date_of_birth && this.person.date_of_birth) {
+                  expect(data.date_of_birth.toJSON()).to.be.equal(this.person.date_of_birth.toJSON());
+                }
                 expect(data.comment).to.be.equal(this.person.comment);
             });
 
     });
 
     Given('the person is in the database', function() {
-        return this.db.one("insert into person (first_name, last_name, title, nickname, date_of_birth, comment) values($1, $2, $3, $4, $5, $6) returning id", [this.person.first_name, this.person.last_name, this.person.title, this.person.nickname, this.person.date_of_birth, this.person.comment])
+        return this.db.one("insert into party (first_name, last_name, title, nickname, date_of_birth, comment, party_type_id) values($1, $2, $3, $4, $5, $6, $7) returning id",
+        [this.person.first_name, this.person.last_name, this.person.title, this.person.nickname, this.person.date_of_birth, this.person.comment, this.party_type_id("Person")])
             .then((data) => this.person.id = data.id)
     });
 
@@ -194,21 +202,21 @@ defineSupportCode(function({
 
     When('I delete the person', function() {
         return this.axios.post('/', {
-            "query": "mutation delete_person($id: ID!) {delete_person(id: $id)}",
-            "variables": {
-                "id": this.person.id
-            },
-            "operationName": "delete_person"
-        })
-        .then((response)=>this.result = response);
+                "query": "mutation delete_person($id: ID!) {delete_person(id: $id)}",
+                "variables": {
+                    "id": this.person.id
+                },
+                "operationName": "delete_person"
+            })
+            .then((response) => this.result = response);
     });
 
-    Then('the person is no longer in the databse', function () {
-         expect(this.result.status).to.be.equal(200);
-         expect(this.result.data.errors).to.be.falsy;
-         expect(this.result.data.data).to.exist;
-         return this.db.one("select id from person where id=$1", [this.person.id])
-         .then((data) => expect(data).to.not.exist)
-         .catch((error)=>expect(error.message).to.be.equal("No data returned from the query."));
-       });
+    Then('the person is no longer in the databse', function() {
+        expect(this.result.status).to.be.equal(200);
+        expect(this.result.data.errors).to.be.falsy;
+        expect(this.result.data.data).to.exist;
+        return this.db.one("select id from party where id=$1", [this.person.id])
+            .then((data) => expect(data).to.not.exist)
+            .catch((error) => expect(error.message).to.be.equal("No data returned from the query."));
+    });
 });
--- graph-schema/index.js
+++ graph-schema/index.js
@@ -6,6 +6,18 @@ import moment from "moment";
 
 import database from "../database";
 
+let contact_mechanism_types = [];
+let party_types = [];
+
+database.any("select id, description from party_type order by description")
+    .then((data) => party_types = data)
+    .then(() => database.any("select id, description from contact_mechanism_type order by description"))
+    .then((data) => contact_mechanism_types = data);
+
+const email_id = () => contact_mechanism_types.find((cm) => cm.description === 'Email Address').id;
+const party_type_id = (party_type) => party_types.find(pt => pt.description === party_type).id;
+const person_type_id = () => party_type_id("Person");
+
 var schema = buildSchema(`
   type Email {
     id: ID!,
@@ -39,7 +51,7 @@ var schema = buildSchema(`
   }
 
   type Mutation {
-    create_person(first_name: String, last_name: String, title: String, nickname: String, date_of_birth: String, comment: String): Person
+    create_person(first_name: String, last_name: String, title: String, nickname: String, date_of_birth: String, comment: String, email: String): Person
     update_person(id: ID!,first_name: String, last_name: String, title: String, nickname: String, date_of_birth: String, comment: String): Person
     delete_person(id: ID!): String
   }
@@ -52,14 +64,26 @@ var root = {
         title,
         nickname,
         date_of_birth,
-        comment
+        comment,
+        email
     }) {
-        return database.one("insert into person (first_name, last_name, title, nickname, date_of_birth, comment) values($1, $2, $3, $4, $5, $6) returning id", [first_name, last_name, title, nickname, date_of_birth, comment])
+        return database
+            .one("insert into party (first_name, last_name, title, nickname, date_of_birth, comment, party_type_id) values($1, $2, $3, $4, $5, $6, $7) returning id",
+                [first_name, last_name, title, nickname, date_of_birth || null, comment, person_type_id()])
+            .then((party_id) => {
+                if (email) {
+                    return database
+                        .one("INSERT INTO contact_mechanism(end_point, contact_mechanism_type_id) VALUES ( $1, $2) returning id", [email, email_id()])
+                        .then((data) => database.one("INSERT INTO party_contact_mechanism(party_id, contact_mechanism_id)VALUES ( $1, $2) returning party_id as id", [party_id.id, data.id]));
+                } else {
+                  return party_id;
+                }
+            })
     },
     delete_person: function({
-      id
-    }){
-      return database.none("delete from person where id = $1", [id]);
+        id
+    }) {
+        return database.none("delete from party where id = $1", [id]);
     },
     organization: function({
         id
@@ -67,12 +91,12 @@ var root = {
         return database.one('select id, name from organization where id=$1', [id]);
     },
     people: () => {
-        return database.any('select id, first_name, last_name, title, nickname, date_of_birth, comment from person');
+        return database.any('select id, first_name, last_name, title, nickname, date_of_birth, comment from party where party_type_id=$1', [person_type_id()]);
     },
     person: function({
         id
     }) {
-        return database.one('select id, first_name, last_name, title, nickname, date_of_birth, comment from person where id=$1', [id]);
+        return database.one('select id, first_name, last_name, title, nickname, date_of_birth, comment from party where id=$1', [id]);
     },
     update_person: function({
         id,
@@ -84,7 +108,7 @@ var root = {
         comment
     }) {
         return database
-            .one("update person set first_name=$1, last_name=$2, title=$3, nickname=$4, date_of_birth=$5, comment=$6 where id=$7 returning id, first_name, last_name, title, nickname, date_of_birth, comment", [first_name, last_name, title, nickname, date_of_birth, comment, id]);
+            .one("update party set first_name=$1, last_name=$2, title=$3, nickname=$4, date_of_birth=$5, comment=$6 where id=$7 returning id, first_name, last_name, title, nickname, date_of_birth, comment", [first_name, last_name, title, nickname, date_of_birth, comment, id]);
     }
 
 };
