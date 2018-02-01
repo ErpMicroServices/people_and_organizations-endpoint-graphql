a88bdd4227706ff4da574e80278a6b03750b546d
Jim Barrows <Jim.Barrows@gmail.com>
2018-01-31 19:34:20 -0600

    It all works now with the graphql yoga setup.

--- Dockerfile
+++ Dockerfile
@@ -11,9 +11,4 @@ RUN npm install --global nodemon babel-core babel-cli
 RUN npm install
 COPY . /usr/src/app
 
-LABEL traefik.backend=party_party_service
-LABEL traefik.frontend.rule=Host:localhost;PathPrefixStrip:/api/party
-LABEL traefik.port=80
-LABEL traefik.frontend.entryPoints=http
-
 CMD [ "nodemon", "index.js", "--exec", "babel-node"]
--- server.js
+++ server.js
@@ -1,43 +1,42 @@
-import {graphiqlExpress, graphqlExpress} from 'apollo-server-express';
-import bodyParser from 'body-parser';
-import express from 'express';
-import {makeExecutableSchema} from 'graphql-tools';
-import config from "./config";
+import {makeExecutableSchema} from 'graphql-tools'
+import {GraphQLServer} from 'graphql-yoga'
+import config from "./config"
 
-import database from "./database";
-import resolvers from "./resolvers";
-import typeDefs from "./type_defs";
+import database from "./database"
+import resolvers from "./resolvers"
+import typeDefs from "./type_defs"
 
-const schema = makeExecutableSchema({typeDefs, resolvers});
+const schema = makeExecutableSchema({typeDefs, resolvers})
 
-const app = express();
+const party_types = new Map()
+const contact_mechanism_types = new Map()
+const person_type_id = () => party_types.get('Person').id
 
-app.use(bodyParser.json());
-
-const party_types             = new Map();
-const contact_mechanism_types = new Map();
-const person_type_id          = () => party_types.get('Person').id;
 
 database.task(t => t.any("select id, description from contact_mechanism_type order by description")
-		.then(data => data.forEach(cmt => contact_mechanism_types.set(cmt.description, cmt)))
-		.then(() => t.any("select id, description, parent_id from party_type order by description"))
-		.then(data => data.forEach(party_type => party_types.set(party_type.description, party_type)))
-).then(() => {
-			app.use('/graphql', graphqlExpress({
-				schema,
-				context: {
-					contact_mechanism_types,
-					database,
-					party_types,
-					person_type_id
-				}
-			}));
-		})
-		.catch(error => console.log("Error: ", error));
-
-
-if (config.graphql.graphiql) {
-	app.use("/graphiql", graphiqlExpress({endpointURL: config.graphql.endpointURL}));
-}
-app.listen(config.server.port, () => console.log('%s listening at %s', config.server.name, config.server.url));
-
+.then(data => data.forEach(cmt => contact_mechanism_types.set(cmt.description, cmt)))
+.then(() => t.any("select id, description, parent_id from party_type order by description"))
+.then(data => data.forEach(party_type => party_types.set(party_type.description, party_type))))
+.then(() => {
+  const server = new GraphQLServer({
+                                     schema: schema,
+                                     context: {
+                                       contact_mechanism_types,
+                                       database,
+                                       party_types,
+                                       person_type_id
+                                     }
+                                   })
+  const options = {
+    cors: config.server.cors,
+    tracing: config.server.tracing,
+    port: config.server.port,
+    endpoint: config.server.endpoint,
+    subscriptions: config.server.subscriptions,
+    playground: config.server.playground,
+    uploads: config.server.uploads
+  }
+  return server.start(options)
+})
+.then(() => console.log('%s is running at url: %s on port %d and the following end points: api: %s, subscriptions: %s, playground: %s', config.server.name, config.server.url, config.server.port, config.server.endpoint, config.server.subscriptions, config.server.playground))
+.catch(error => console.log("Error: ", error))
