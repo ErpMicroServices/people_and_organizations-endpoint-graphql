a005c08494ddaa4300bd5b86a46521524fe8de7c
Jim Barrows <JIm.Barrows@gmail.com>
2018-08-02 21:06:48 -0500

    Party tables and admin done

--- graphql_api/models/admin.py
+++ graphql_api/models/admin.py
@@ -1,9 +1,25 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(PartyType)
 admin.site.register(ClassificationType)
-admin.site.register(RoleType)
-admin.site.register(RelationshipType)
+admin.site.register(PartyType)
 admin.site.register(PriorityType)
 admin.site.register(RelationshipStatusType)
+admin.site.register(RelationshipType)
+admin.site.register(RoleType)
+
+class PartyClassificationInline(admin.TabularInline):
+    model = PartyClassification
+
+class PartyRoleInline(admin.TabularInline):
+    model = PartyRole
+
+class PartyAdmin(admin.ModelAdmin):
+    inlines = (PartyClassificationInline, PartyRoleInline,)
+    list_display = ('name', 'last_name', 'first_name', 'title')
+
+class PartyRelationshipAdmin(admin.ModelAdmin):
+    list_display = ('from_date', 'thru_date', 'from_party', 'to_party', 'type','status','priority')
+
+admin.site.register(Party, PartyAdmin)
+admin.site.register(PartyRelationship,PartyRelationshipAdmin)
--- graphql_api/models/models.py
+++ graphql_api/models/models.py
@@ -1,29 +1,68 @@
 from django.db import models
 import uuid
 
-# Create your models here.
+class ClassificationType(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    description = models.CharField(blank=False, null=False, unique=True, max_length=255)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, blank=True, null=True)
+    def __str__(self):
+        return self.description
 
 class PartyType(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     description = models.CharField(blank=False, null=False, unique=True, max_length=255)
-    parent = models.ForeignKey('self', on_delete=models.CASCADE)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, blank=True, null=True)
+    def __str__(self):
+        return self.description
+
+class PriorityType(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    description = models.CharField(blank=False, null=False, unique=True, max_length=255)
+    parent = models.ForeignKey('self',  blank=True, null=True, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.description
+
+class RelationshipStatusType(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    description = models.CharField(blank=False, null=False, unique=True, max_length=255)
+    parent = models.ForeignKey('self', blank=True, null=True, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.description
+
+class RoleType(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    description = models.CharField(blank=False, null=False, unique=True, max_length=255)
+    parent = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.description
+
+class RelationshipType(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    description = models.CharField(blank=False, null=False, unique=True, max_length=255)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
+    required_from_role_type = models.ForeignKey(RoleType, on_delete=models.PROTECT, related_name='required_from_role_type')
+    required_to_role_type = models.ForeignKey(RoleType, on_delete=models.PROTECT, related_name='required_to_role_type')
+    def __str__(self):
+        return self.description
 
 class Party(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    government_id = models.CharField(max_length=255)
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
-    name = models.CharField( max_length=255)
-    title = models.CharField( max_length=255)
-    nickname = models.CharField( max_length=255)
-    date_of_birth = models.DateField()
-    comment = models.TextField()
+    government_id = models.CharField(max_length=255, blank=True, null=True)
+    first_name = models.CharField(max_length=255, blank=True, null=True)
+    last_name = models.CharField(max_length=255, blank=True, null=True)
+    name = models.CharField( max_length=255, blank=True, null=True)
+    title = models.CharField( max_length=255, blank=True, null=True)
+    nickname = models.CharField( max_length=255, blank=True, null=True)
+    date_of_birth = models.DateField(blank=True, null=True)
+    comment = models.TextField( blank=True, null=True)
     party_type = models.ForeignKey(PartyType, on_delete=models.PROTECT)
-
-class ClassificationType(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    description = models.CharField(blank=False, null=False, unique=True, max_length=255)
-    parent = models.ForeignKey('self', on_delete=models.CASCADE)
+    classifications = models.ManyToManyField(ClassificationType, through='PartyClassification')
+    roles = models.ManyToManyField(RoleType, through='PartyRole')
+    def full_name(self):
+        if (self.first_name is not None) or (self.last_name is not None):
+            return '%s %s' % (self.last_name, self.first_name)
+        else:
+            return self.name
 
 class PartyClassification(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
@@ -31,44 +70,25 @@ class PartyClassification(models.Model):
     from_date = models.DateTimeField(auto_now_add=True, blank=False, null=False)
     thru_date = models.DateTimeField( blank=True, null=True)
     type = models.ForeignKey(ClassificationType, on_delete=models.PROTECT)
-
-class RoleType(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    description = models.CharField(blank=False, null=False, unique=True, max_length=255)
-    parent = models.ForeignKey('self', on_delete=models.CASCADE)
+    party = models.ForeignKey(Party, on_delete=models.CASCADE)
 
 class PartyRole(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    value = models.TextField(blank=False)
+    value = models.TextField(blank=True, null=True)
     from_date = models.DateTimeField(auto_now_add=True, blank=False, null=False)
     thru_date = models.DateTimeField( blank=True, null=True)
     type = models.ForeignKey(RoleType, on_delete=models.PROTECT)
     party = models.ForeignKey(Party, on_delete=models.CASCADE)
-
-class RelationshipType(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    description = models.CharField(blank=False, null=False, unique=True, max_length=255)
-    parent = models.ForeignKey('self', on_delete=models.CASCADE)
-    from_role_type = models.ForeignKey(RoleType, on_delete=models.PROTECT, related_name='from_role_type')
-    to_role_type = models.ForeignKey(RoleType, on_delete=models.PROTECT, related_name='to_role_type')
-
-class PriorityType(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    description = models.CharField(blank=False, null=False, unique=True, max_length=255)
-    parent = models.ForeignKey('self', on_delete=models.CASCADE)
-
-class RelationshipStatusType(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    description = models.CharField(blank=False, null=False, unique=True, max_length=255)
-    parent = models.ForeignKey('self', on_delete=models.CASCADE)
+    def __str__(self):
+        return "%s:%s" %(self.type.description, self.party.full_name())
 
 class PartyRelationship(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     from_date = models.DateTimeField(auto_now_add=True, blank=False, null=False)
     thru_date = models.DateTimeField( blank=True, null=True)
-    comment = models.TextField()
-    from_party = models.ForeignKey(Party, on_delete=models.PROTECT, related_name='from_party')
-    to_party = models.ForeignKey(Party, on_delete=models.PROTECT, related_name='to_party')
+    comment = models.TextField( blank=True, null=True)
+    from_party = models.ForeignKey(PartyRole, on_delete=models.PROTECT, related_name='from_party')
+    to_party = models.ForeignKey(PartyRole, on_delete=models.PROTECT, related_name='to_party')
     type = models.ForeignKey(RelationshipType, on_delete=models.PROTECT)
     status = models.ForeignKey(RelationshipStatusType, on_delete= models.PROTECT)
     priority = models.ForeignKey(PriorityType, on_delete = models.PROTECT)
