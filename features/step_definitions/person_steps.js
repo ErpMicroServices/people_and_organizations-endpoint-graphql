40ab55c14b14c947862e7a283645b2c65c5c8db9
Jim Barrows <JIm.Barrows@gmail.com>
2018-11-11 11:47:30 -0600

    Save person works.

--- features/step_definitions/person_steps.js
+++ features/step_definitions/person_steps.js
@@ -237,4 +237,63 @@ defineSupportCode(function ({
 		expect(data.comment).to.be.equal(this.party.comment)
 		callback()
 	})
+
+	When('I save the person', function () {
+		return this.client
+		.mutate({
+			mutation : gql`mutation create_person(
+      $first_name: String
+      $last_name: String
+      $title: String
+      $nickname: String
+      $date_of_birth: String
+      $comment: String
+      $email: String
+      ) {
+        create_person(
+          first_name: $first_name
+          last_name: $last_name
+          title: $title
+          nickname: $nickname
+          date_of_birth: $date_of_birth
+          comment: $comment
+          email: $email
+        ) {
+          id
+        }
+      }`,
+			variables: {
+				"first_name"   : this.party.first_name,
+				"last_name"    : this.party.last_name,
+				"title"        : this.party.title,
+				"nickname"     : this.party.nickname,
+				"date_of_birth": this.party.date_of_birth ? this.party.date_of_birth.toJSON() : null,
+				"comment"      : this.party.comment,
+				"email"        : this.party.email
+			}
+		})
+		.then(results => this.result.data = results)
+		.catch(error => this.result.error = error)
+
+	})
+
+	Then('the person data will be in the database', function () {
+		expect(this.result.error).to.be.null
+		expect(this.result.data.data).to.exist
+		expect(this.result.data.data.create_person).to.exist
+		expect(this.result.data.data.create_person.id).to.exist
+		return this.db.one("select id, first_name, last_name, title, nickname, date_of_birth, comment from party where id=$1", [this.result.data.data.create_person.id])
+			.then(data => {
+				expect(data.id).to.be.equal(this.result.data.data.create_person.id)
+				expect(data.first_name).to.be.equal(this.party.first_name)
+				expect(data.last_name).to.be.equal(this.party.last_name)
+				expect(data.title).to.be.equal(this.party.title)
+				expect(data.nickname).to.be.equal(this.party.nickname)
+				if (data.date_of_birth && this.party.date_of_birth) {
+					expect(data.date_of_birth.toJSON()).to.be.equal(this.party.date_of_birth.toJSON())
+				}
+				expect(data.comment).to.be.equal(this.party.comment)
+			})
+
+	})
 })
