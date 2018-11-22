13439178b99d7c5ed5fa6f523666cc1327175b64
Jim Barrows <JIm.Barrows@gmail.com>
2018-11-22 09:22:14 -0600

    Removed the python stuff.

--- communication/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from graphene_django.views import GraphQLView
-
-from . import schema
-
-urlpatterns = [
-    url(r'^graphql', GraphQLView.as_view(graphiql=True, schema=schema)),
-]
--- facility/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import *
-
-admin.site.register(FacilityType)
-admin.site.register(FacilityRoleType)
-admin.site.register(Facility)
-admin.site.register(FacilityRole)
-admin.site.register(FacilityContactMechanism)
