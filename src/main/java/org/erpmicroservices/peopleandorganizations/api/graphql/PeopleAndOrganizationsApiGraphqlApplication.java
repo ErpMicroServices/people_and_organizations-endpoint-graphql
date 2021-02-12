c9314ff58a8821ac2ec00235a65789b052672d8d
Jim Barrows <Jim.Barrows@gmail.com>
2021-02-12 16:40:05 -0600

    Adding in the maven project.

---
+++ src/main/java/org/erpmicroservices/peopleandorganizations/api/graphql/PeopleAndOrganizationsApiGraphqlApplication.java
@@ -0,0 +1,13 @@
+package org.erpmicroservices.peopleandorganizations.api.graphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeopleAndOrganizationsApiGraphqlApplication {
+
+ public static void main(String[] args) {
+	SpringApplication.run(PeopleAndOrganizationsApiGraphqlApplication.class, args);
+ }
+
+}
