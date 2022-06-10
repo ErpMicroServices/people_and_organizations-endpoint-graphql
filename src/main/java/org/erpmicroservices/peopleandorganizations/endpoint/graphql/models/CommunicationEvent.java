585739d568e2cd0da70dbeffe2c2505dd1278623
Jim Barrows <Jim.Barrows@gmail.com>
2022-06-10 11:40:16 -0500

    Added mre annotations

--- src/main/java/org/erpmicroservices/peopleandorganizations/endpoint/graphql/models/CommunicationEvent.java
+++ src/main/java/org/erpmicroservices/peopleandorganizations/endpoint/graphql/models/CommunicationEvent.java
@@ -1,18 +1,22 @@
 package org.erpmicroservices.peopleandorganizations.endpoint.graphql.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.util.UUID;
 
 @Entity
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CommunicationEvent {
 	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	@NotNull
 	private UUID id;
 
