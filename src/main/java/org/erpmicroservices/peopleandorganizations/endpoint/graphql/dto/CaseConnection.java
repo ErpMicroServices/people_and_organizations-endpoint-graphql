759cea5c67ab1d516e91872160784be822ba2aa9
Jim Barrows <Jim.Barrows@gmail.com>
2022-06-11 21:45:50 -0500

    Cases return a connection now

--- src/main/java/org/erpmicroservices/peopleandorganizations/endpoint/graphql/dto/CaseConnection.java
+++ src/main/java/org/erpmicroservices/peopleandorganizations/endpoint/graphql/dto/CaseConnection.java
@@ -5,22 +5,22 @@ import graphql.relay.Edge;
 import graphql.relay.PageInfo;
 import lombok.Builder;
 import lombok.Data;
-import org.erpmicroservices.peopleandorganizations.endpoint.graphql.models.Party;
+import org.erpmicroservices.peopleandorganizations.endpoint.graphql.models.Case;
 
 import java.util.List;
 
 @Data
 @Builder
-public class PartyConnection implements Connection<Party> {
+public class CaseConnection implements Connection<Case> {
 
-	private List<Edge<Party>> edges;
+	private List<Edge<Case>> edges;
 	private PageInfo pageInfo;
 
 	/**
 	 * @return a list of {@link Edge}s that are really a node of data and its cursor
 	 */
 	@Override
-	public List<Edge<Party>> getEdges() {
+	public List<Edge<Case>> getEdges() {
 		return edges;
 	}
 
