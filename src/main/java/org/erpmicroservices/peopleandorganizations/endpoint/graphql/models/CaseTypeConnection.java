720b38aad29bac12a79ac9e0c3529aa4ee3cb48c
Jim Barrows <Jim.Barrows@gmail.com>
2022-06-28 07:17:00 -0500

    Case Types returns connections

---
+++ src/main/java/org/erpmicroservices/peopleandorganizations/endpoint/graphql/models/CaseTypeConnection.java
@@ -0,0 +1,33 @@
+package org.erpmicroservices.peopleandorganizations.endpoint.graphql.models;
+
+import graphql.relay.Edge;
+import lombok.Builder;
+import lombok.Data;
+import org.erpmicroservices.peopleandorganizations.endpoint.graphql.dto.Connection;
+import org.erpmicroservices.peopleandorganizations.endpoint.graphql.dto.PageInfo;
+
+import java.util.List;
+
+@Data
+@Builder
+public class CaseTypeConnection implements Connection<CaseType> {
+
+	private List<Edge<CaseType>> edges;
+	private PageInfo pageInfo;
+
+	/**
+	 * @return a list of {@link Edge}s that are really a node of data and its cursor
+	 */
+	@Override
+	public List<Edge<CaseType>> getEdges() {
+		return edges;
+	}
+
+	/**
+	 * @return {@link PageInfo} pagination data about that list of edges
+	 */
+	@Override
+	public PageInfo getPageInfo() {
+		return pageInfo;
+	}
+}
--- src/main/java/org/erpmicroservices/peopleandorganizations/endpoint/graphql/repositories/CaseTypeRepository.java
+++ src/main/java/org/erpmicroservices/peopleandorganizations/endpoint/graphql/repositories/CaseTypeRepository.java
@@ -1,9 +1,14 @@
 package org.erpmicroservices.peopleandorganizations.endpoint.graphql.repositories;
 
 import org.erpmicroservices.peopleandorganizations.endpoint.graphql.models.CaseType;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.UUID;
 
-public interface CaseTypeRepository extends CrudRepository<CaseType, UUID> {
+public interface CaseTypeRepository extends PagingAndSortingRepository<CaseType, UUID> {
+	Page<CaseType> findCaseTypeByParentEquals(CaseType parent, Pageable pageable);
+
+	Page<CaseType> findCaseTypeByParentIsNull(final Pageable pageable);
 }
