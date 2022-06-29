dd8bcf82362408108b61b806e4e06059c46dfa5b
Jim Barrows <Jim.Barrows@gmail.com>
2022-06-29 16:37:39 -0500

    CommunicationEventRoleTypeController now returns connections

--- src/main/java/org/erpmicroservices/peopleandorganizations/endpoint/graphql/repositories/CommunicationEventRoleTypeRepository.java
+++ src/main/java/org/erpmicroservices/peopleandorganizations/endpoint/graphql/repositories/CommunicationEventRoleTypeRepository.java
@@ -1,6 +1,8 @@
 package org.erpmicroservices.peopleandorganizations.endpoint.graphql.repositories;
 
 import org.erpmicroservices.peopleandorganizations.endpoint.graphql.models.CommunicationEventRoleType;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.graphql.data.GraphQlRepository;
 
@@ -8,5 +10,7 @@ import java.util.UUID;
 
 @GraphQlRepository
 public interface CommunicationEventRoleTypeRepository extends PagingAndSortingRepository<CommunicationEventRoleType, UUID> {
+	Page<CommunicationEventRoleType> findCommunicationEventRoleTypeByParentEquals(CommunicationEventRoleType parent, Pageable pageable);
 
+	Page<CommunicationEventRoleType> findCommunicationEventRoleTypeByParentIsNull(final Pageable pageable);
 }
