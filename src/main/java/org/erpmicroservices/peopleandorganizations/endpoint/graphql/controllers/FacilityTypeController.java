fdc806c5d0da74484690265b1f412ccee4f94f13
Jim Barrows <Jim.Barrows@gmail.com>
2022-06-30 17:37:23 -0500

    Facility Type returns a connection

--- src/main/java/org/erpmicroservices/peopleandorganizations/endpoint/graphql/controllers/FacilityTypeController.java
+++ src/main/java/org/erpmicroservices/peopleandorganizations/endpoint/graphql/controllers/FacilityTypeController.java
@@ -1,11 +1,22 @@
 package org.erpmicroservices.peopleandorganizations.endpoint.graphql.controllers;
 
+import graphql.relay.Edge;
+import org.erpmicroservices.peopleandorganizations.endpoint.graphql.dto.Cursor;
+import org.erpmicroservices.peopleandorganizations.endpoint.graphql.dto.FacilityTypeConnection;
+import org.erpmicroservices.peopleandorganizations.endpoint.graphql.dto.FacilityTypeEdge;
+import org.erpmicroservices.peopleandorganizations.endpoint.graphql.dto.PageInfo;
 import org.erpmicroservices.peopleandorganizations.endpoint.graphql.models.FacilityType;
 import org.erpmicroservices.peopleandorganizations.endpoint.graphql.repositories.FacilityTypeRepository;
+import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.stereotype.Controller;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.lang.String.valueOf;
+import static org.erpmicroservices.peopleandorganizations.endpoint.graphql.misc.Utils.pageInfoToPageable;
 
 
 @Controller
@@ -18,7 +29,30 @@ public class FacilityTypeController {
 	}
 
 	@QueryMapping
-	public List<FacilityType> facilityTypes() {
-		return (List<FacilityType>) repository.findAll();
+	public FacilityTypeConnection facilityTypes(@Argument PageInfo pageInfo) {
+		final List<Edge<FacilityType>> edges = repository.findFacilityTypesByParentIsNull(pageInfoToPageable(pageInfo)).stream()
+				                                       .map(facilityType -> FacilityTypeEdge.builder()
+						                                                            .node(facilityType)
+						                                                            .cursor(Cursor.builder().value(valueOf(facilityType.getId().hashCode())).build())
+						                                                            .build())
+				                                       .collect(Collectors.toList());
+		return FacilityTypeConnection.builder()
+				       .edges(edges)
+				       .pageInfo(pageInfo)
+				       .build();
+	}
+
+	@SchemaMapping
+	public FacilityTypeConnection children(FacilityType parent, @Argument PageInfo pageInfo) {
+		final List<Edge<FacilityType>> edges = repository.findFacilityTypesByParentEquals(parent, pageInfoToPageable(pageInfo)).stream()
+				                                       .map(facilityType -> FacilityTypeEdge.builder()
+						                                                            .node(facilityType)
+						                                                            .cursor(Cursor.builder().value(valueOf(facilityType.getId().hashCode())).build())
+						                                                            .build())
+				                                       .collect(Collectors.toList());
+		return FacilityTypeConnection.builder()
+				       .edges(edges)
+				       .pageInfo(pageInfo)
+				       .build();
 	}
 }
--- src/main/java/org/erpmicroservices/peopleandorganizations/endpoint/graphql/repositories/FacilityTypeRepository.java
+++ src/main/java/org/erpmicroservices/peopleandorganizations/endpoint/graphql/repositories/FacilityTypeRepository.java
@@ -1,6 +1,8 @@
 package org.erpmicroservices.peopleandorganizations.endpoint.graphql.repositories;
 
 import org.erpmicroservices.peopleandorganizations.endpoint.graphql.models.FacilityType;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.graphql.data.GraphQlRepository;
 
@@ -9,4 +11,7 @@ import java.util.UUID;
 @GraphQlRepository
 public interface FacilityTypeRepository extends PagingAndSortingRepository<FacilityType, UUID> {
 
+	Page<FacilityType> findFacilityTypesByParentEquals(final FacilityType parent, final Pageable pageable);
+
+	Page<FacilityType> findFacilityTypesByParentIsNull(final Pageable pageable);
 }
