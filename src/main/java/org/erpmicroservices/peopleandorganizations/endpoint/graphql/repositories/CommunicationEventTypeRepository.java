0404545f0cc5409297a6222b8bbeaaf3f98babb4
Jim Barrows <Jim.Barrows@gmail.com>
2022-06-29 19:20:25 -0500

    CommunicationEventTypeController now returns connections

--- src/main/java/org/erpmicroservices/peopleandorganizations/endpoint/graphql/repositories/CommunicationEventTypeRepository.java
+++ src/main/java/org/erpmicroservices/peopleandorganizations/endpoint/graphql/repositories/CommunicationEventTypeRepository.java
@@ -1,12 +1,17 @@
 package org.erpmicroservices.peopleandorganizations.endpoint.graphql.repositories;
 
 import org.erpmicroservices.peopleandorganizations.endpoint.graphql.models.CommunicationEventType;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.graphql.data.GraphQlRepository;
 
 import java.util.UUID;
 
 @GraphQlRepository
-public interface CommunicationEventTypeRepository extends CrudRepository<CommunicationEventType, UUID> {
+public interface CommunicationEventTypeRepository extends PagingAndSortingRepository<CommunicationEventType, UUID> {
 
+	Page<CommunicationEventType> findCommunicationEventTypeByParentEquals(CommunicationEventType parent, Pageable pageable);
+
+	Page<CommunicationEventType> findCommunicationEventTypeByParentIsNull(final Pageable pageable);
 }
