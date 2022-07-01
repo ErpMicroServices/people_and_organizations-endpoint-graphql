374c29ba41149651e46ef458ec1bdcb87d87310f
Jim Barrows <Jim.Barrows@gmail.com>
2022-06-30 19:27:24 -0500

    Party Type returns connections

--- src/main/java/org/erpmicroservices/peopleandorganizations/endpoint/graphql/controllers/PartyTypeController.java
+++ src/main/java/org/erpmicroservices/peopleandorganizations/endpoint/graphql/controllers/PartyTypeController.java
@@ -1,11 +1,22 @@
 package org.erpmicroservices.peopleandorganizations.endpoint.graphql.controllers;
 
+import graphql.relay.Edge;
+import org.erpmicroservices.peopleandorganizations.endpoint.graphql.dto.Cursor;
+import org.erpmicroservices.peopleandorganizations.endpoint.graphql.dto.PageInfo;
+import org.erpmicroservices.peopleandorganizations.endpoint.graphql.dto.PartyTypeConnection;
+import org.erpmicroservices.peopleandorganizations.endpoint.graphql.dto.PartyTypeEdge;
 import org.erpmicroservices.peopleandorganizations.endpoint.graphql.models.PartyType;
 import org.erpmicroservices.peopleandorganizations.endpoint.graphql.repositories.PartyTypeRepository;
+import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.stereotype.Controller;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.lang.String.valueOf;
+import static org.erpmicroservices.peopleandorganizations.endpoint.graphql.misc.Utils.pageInfoToPageable;
 
 @Controller
 public class PartyTypeController {
@@ -17,7 +28,30 @@ public class PartyTypeController {
 	}
 
 	@QueryMapping
-	public List<PartyType> partyTypes() {
-		return (List<PartyType>) partyTypeRepository.findAll();
+	public PartyTypeConnection partyTypes(@Argument PageInfo pageInfo) {
+		final List<Edge<PartyType>> edges = partyTypeRepository.findPartyTypesByParentIsNull(pageInfoToPageable(pageInfo)).stream()
+				                                    .map(partyType -> PartyTypeEdge.builder()
+						                                                      .node(partyType)
+						                                                      .cursor(Cursor.builder().value(valueOf(partyType.getId().hashCode())).build())
+						                                                      .build())
+				                                    .collect(Collectors.toList());
+		return PartyTypeConnection.builder()
+				       .edges(edges)
+				       .pageInfo(pageInfo)
+				       .build();
+	}
+
+	@SchemaMapping
+	public PartyTypeConnection children(PartyType parent, @Argument PageInfo pageInfo) {
+		final List<Edge<PartyType>> edges = partyTypeRepository.findPartyTypesByParentEquals(parent, pageInfoToPageable(pageInfo)).stream()
+				                                    .map(partyType -> PartyTypeEdge.builder()
+						                                                      .node(partyType)
+						                                                      .cursor(Cursor.builder().value(valueOf(partyType.getId().hashCode())).build())
+						                                                      .build())
+				                                    .collect(Collectors.toList());
+		return PartyTypeConnection.builder()
+				       .edges(edges)
+				       .pageInfo(pageInfo)
+				       .build();
 	}
 }
--- src/main/java/org/erpmicroservices/peopleandorganizations/endpoint/graphql/repositories/PartyTypeRepository.java
+++ src/main/java/org/erpmicroservices/peopleandorganizations/endpoint/graphql/repositories/PartyTypeRepository.java
@@ -1,6 +1,8 @@
 package org.erpmicroservices.peopleandorganizations.endpoint.graphql.repositories;
 
 import org.erpmicroservices.peopleandorganizations.endpoint.graphql.models.PartyType;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.graphql.data.GraphQlRepository;
 
@@ -8,4 +10,7 @@ import java.util.UUID;
 
 @GraphQlRepository
 public interface PartyTypeRepository extends CrudRepository<PartyType, UUID> {
+	Page<PartyType> findPartyTypesByParentEquals(final PartyType parent, final Pageable pageable);
+
+	Page<PartyType> findPartyTypesByParentIsNull(final Pageable pageable);
 }
