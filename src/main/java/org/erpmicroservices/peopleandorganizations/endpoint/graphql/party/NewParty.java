0ae717a7dadb3324ffde6108b14104173c741edc
Jim Barrows <Jim.Barrows@gmail.com>
2022-07-04 09:15:22 -0500

    Added validation annotation

--- src/main/java/org/erpmicroservices/peopleandorganizations/endpoint/graphql/party/NewParty.java
+++ src/main/java/org/erpmicroservices/peopleandorganizations/endpoint/graphql/party/NewParty.java
@@ -3,11 +3,13 @@ package org.erpmicroservices.peopleandorganizations.endpoint.graphql.party;
 import lombok.Builder;
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
 import java.util.UUID;
 
 @Data
 @Builder
 public class NewParty {
 	private String comment;
+	@NotNull
 	private UUID partyTypeId;
 }
