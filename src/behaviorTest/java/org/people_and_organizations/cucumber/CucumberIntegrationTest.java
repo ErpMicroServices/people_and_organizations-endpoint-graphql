6aa58d59c78c6fae0d2ae9eb0365bd50a16c2e20
Jim Barrows <JIm.Barrows@gmail.com>
2023-06-03 11:06:01 -0500

    Cucumber is setup to run wit gradle as a behavior test

--- src/behaviorTest/java/org/people_and_organizations/cucumber/CucumberIntegrationTest.java
+++ src/behaviorTest/java/org/people_and_organizations/cucumber/CucumberIntegrationTest.java
@@ -4,7 +4,10 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
-@RunWith( Cucumber.class )
-//@CucumberOptions(features = "src/test/resources")
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		plugin = {"pretty", "html:target/cucumber-report.html"},
+		features = {"src/behaviorTest/resources"}
+)
 public class CucumberIntegrationTest {
 }
