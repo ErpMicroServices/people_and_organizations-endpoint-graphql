a5bbe0856750d55214c7bd3ae3c97a29f69b4d97
Jim Barrows <JIm.Barrows@gmail.com>
2023-06-03 11:08:28 -0500

    25 convert to gradle (#26)
    * Cucumber is setup to run wit gradle as a behavior test
    
    * Cucumber is setup to run wit gradle as a behavior test
    
    * Cucumber is setup to run wit gradle as a behavior test

--- src/behaviorTest/java/org/people_and_organizations/cucumber/SpringIntegrationTest.java
+++ src/behaviorTest/java/org/people_and_organizations/cucumber/SpringIntegrationTest.java
@@ -1,9 +1,7 @@
 package org.people_and_organizations.cucumber;
 
-import io.cucumber.spring.CucumberContextConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@CucumberContextConfiguration
 @SpringBootTest
 public class SpringIntegrationTest {
 }
